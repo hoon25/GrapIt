7cb299dd2e869b3dabdbe28c367d95b7af0e6222
hoon25 <cchoon95@hanmail.net>
2023-01-07 00:50:57 +0900

    [#1] docs : Add PR-template

---
+++ grapit-spring/src/test/java/edu/oak/grapitspring/GrapitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.oak.grapitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

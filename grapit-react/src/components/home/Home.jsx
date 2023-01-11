bf68c9cff69b78e74d6ddbde308b749dac7945e8
haein <oeanin2088@gmail.com>
2023-01-11 22:33:36 +0900

    [#25] feat : 메인페이지 추가

---
+++ grapit-react/src/components/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Featured from "./featured/Featured"
+import Hero from "./hero/Hero"
+import Footer from "../common/footer/Footer"
+
+
+
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Featured />
+      <Footer />
+      
+    </>
+  )
+}
+
+export default Home

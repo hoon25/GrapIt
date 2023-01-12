efac4fa2be21875d26527db49ad312f48e92973f
hoon25 <cchoon95@hanmail.net>
2023-01-13 06:04:16 +0900

    [#23] feat : Socket서버 초기 구축
    환경변수 설정
    
    resolves : #23

---
+++ grapit-spring-socket/src/test/java/edu/oak/grapitsocket/GrapitSpringSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.oak.grapitsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapitSpringSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
